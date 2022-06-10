28f918b6f23df16bbf484b25d979f8f01464c065
yushenlin <byushenlin@h-tech.co>
2022-06-10 17:32:14 +0800

    [feature] Create site_videos table

---
+++ migrations/20220610073759_create_site_videos_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+class CreateSiteVideosTable extends AbstractMigration
+{
+    private $tableName = 'site_videos';
+
+    public function up()
+    {
+        $table = $this->table($this->tableName, [
+            'id' => false,
+            'primary_key' => 'id',
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => '站台影片',
+        ]);
+
+        $table->addColumn('id', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'identity' => true])
+            ->addColumn('video_id', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'comment' => '片庫影片id'])
+            ->addColumn('site_id', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'comment' => 'mapping sites.id'])
+            ->addColumn('category_id', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'comment' => 'mapping categories.id'])
+            ->addColumn('display', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'signed' => false, 'comment' => '是否顯示'])
+            ->addColumn('status', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'signed' => false, 'comment' => '狀態 Video/Status'])
+            ->addColumn('title', 'string', ['limit' => 255, 'comment' => '標題'])
+            ->addColumn('title_en', 'string', ['limit' => 255, 'null' => true, 'comment' => '英文標題'])
+            ->addColumn('cover', 'string', ['limit' => 255, 'null' => true, 'comment' => '封面'])
+            ->addColumn('order', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '排序'])
+            ->addColumn('play', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '灌水點播數'])
+            ->addColumn('play_real', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '實際點播數'])
+            ->addColumn('like', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '灌水點讚數'])
+            ->addColumn('live_real', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '實際點讚數'])
+            ->addColumn('favorite', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '灌水收藏數'])
+            ->addColumn('favorite_real', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '實際收藏數'])
+            ->addColumn('type', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '收費方式 Video/Type'])
+            ->addColumn('price', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '售價'])
+            ->addColumn('buy', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '灌水購買數'])
+            ->addColumn('buy_real', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '實際購買數'])
+            ->addColumn('create_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'comment' => '建立時間'])
+            ->addColumn('update_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP', 'comment' => '更新時間'])
+            ->save();
+
+        $table->addIndex(['video_id', 'site_id'], ['unique' => true, 'name' => 'idx_video_id_site_id_unique'])
+            ->addIndex(['site_id', 'category_id'], ['name' => 'idx_site_id_category_id'])
+            ->save();
+    }
+
+    public function down()
+    {
+        if ($this->hasTable($this->tableName)) {
+            $this->table($this->tableName)->drop()->save();
+        }
+    }
+}
