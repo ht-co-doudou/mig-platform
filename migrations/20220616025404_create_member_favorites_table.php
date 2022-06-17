36ea3a885b8fc76b527aecdbad45c763bc9fc3fa
yushenlin <byushenlin@h-tech.co>
2022-06-17 10:39:59 +0800

    [feature] 新增會員收藏、點讚資料表

---
+++ migrations/20220616025404_create_member_favorites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+class CreateMemberFavoritesTable extends AbstractMigration
+{
+    private $tableName = 'member_favorites';
+
+    public function up()
+    {
+        $table = $this->table($this->tableName, [
+            'id' => false,
+            'primary_key' => ['member_id', 'site_video_id'],
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => '會員收藏的影片',
+        ]);
+
+        $table->addColumn('member_id', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'comment' => 'mapping members.id'])
+            ->addColumn('site_video_id', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'comment' => 'mapping site_videos.id'])
+            ->addColumn('create_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'comment' => '建立時間'])
+            ->save();
+
+        $table->addForeignKey('member_id', 'members', 'id', ['constraint' => "fk_{$this->tableName}_member_id"])
+            ->addForeignKey('site_video_id', 'site_videos', 'id', ['constraint' => "fk_{$this->tableName}_video_id"])
+            ->save();
+    }
+
+    public function down()
+    {
+        if ($this->hasTable($this->tableName)) {
+            $this->table($this->tableName)->drop()->save();
+        }
+    }
+}
