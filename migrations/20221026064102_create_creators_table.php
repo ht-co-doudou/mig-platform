4c8529d287d4fcd8adb57417d8a68ec0a7d34839
Nolions <bnolions@ht-tech.co>
2022-10-27 11:33:05 +0800

    [revsie] 調整欄位註解

--- migrations/20221026064102_create_creators_table.php
+++ migrations/20221026064102_create_creators_table.php
@@ -22,8 +22,7 @@ class CreateCreatorsTable extends AbstractMigration
             ->addColumn('describe', 'string', ['limit' => 255, 'null' => true, 'comment' => '創作者描述'])
             ->addColumn('fans', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '粉絲數'])
             ->addColumn('follow', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0, 'comment' => '追縱數'])
-            ->addColumn('type', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'signed' => false, 'comment' => '類型 Audit/Type'])
-            ->addColumn('status', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'signed' => false, 'comment' => '狀態 Audit/Status'])
+            ->addColumn('status', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'signed' => false, 'comment' => '狀態'])
             ->addColumn('create_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'comment' => '建立時間'])
             ->addColumn('update_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP', 'comment' => '更新時間'])
             ->save();
