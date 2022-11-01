2f30624c1ca040ce1aa9a038cebf9fa5d2c1d5d8
Nolions <bnolions@ht-tech.co>
2022-11-01 18:03:22 +0800

    [revise] creator_follow 欄位型態改為integer

--- migrations/20221027063910_add_creator_follow_to_members_table.php
+++ migrations/20221027063910_add_creator_follow_to_members_table.php
@@ -9,7 +9,7 @@ class AddCreatorFollowToMembersTable extends AbstractMigration
     public function up()
     {
         $this->table($this->tableName)
-            ->addColumn('creator_follow', 'decimal', ['precision' => 16, 'scale' => 4, 'default' => 0, 'comment' => '追蹤創作者數', 'after' => 'delete_at'])
+            ->addColumn('creator_follow', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'signed' => false, 'default' => 0 ,'comment' => '追蹤創作者數', 'after' => 'delete_at'])
             ->save();
     }
 
